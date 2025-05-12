c8519172ba1e16a4401dc7051f60e1d0fdc90464
rda-cunh <ricard.mendes@gmail.com>
2025-05-13 00:13:08 +0100

    completed ex00 and corrected makefiles

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,26 +6,31 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 17:40:59 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/05/10 19:54:26 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/05/13 00:07:22 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-namespace   Math
+void    megaphone(char *str)
 {
-    int add(int a, int b) { return (a + b); }
-    int substract(int a, int b) { return (a - b); }
-    int multiply(int a, int b) { return (a * b); }
-    int divide(int a, int b) { return (a / b); }
+    while(*str)
+        std::cout << (char)std::toupper(*str++);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "*** Operations with 6 and 2 ***" << std::endl;
-    std::cout << "Add: " << Math::add(6, 2) << std::endl;  
-    std::cout << "Substract: " << Math::substract(6, 2) << std::endl;
-    std::cout << "Multiply: " << Math::multiply(6, 2) << std::endl;
-    std::cout << "Divide: " << Math::divide(6, 2) << std::endl;
-    return (0);
+    int i = 1;
+    if (argc > 1)
+    {
+        while (i < argc)
+        {
+            megaphone(argv[i]);
+            i++;
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+    return (0); 
 }
