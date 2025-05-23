1df8c21843c9992f7781c496392b115e733e34de
rda-cunh <ricard.mendes@gmail.com>
2025-05-23 01:06:06 +0100

    work on ex03

--- module_01/ex03/Weapon.hpp
+++ module_01/ex03/Weapon.hpp
@@ -6,7 +6,24 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 01:18:01 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/05/22 01:18:06 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/05/23 01:05:25 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+#include <iostream>
+#include <string>
+
+class Weapon
+{
+    private:
+        std::string _type;
+    
+    public:
+        Weapon()    
+
+};
+
+#endif
--- module_01/ex03/main.cpp
+++ module_01/ex03/main.cpp
@@ -6,7 +6,31 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 01:18:51 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/05/22 01:18:56 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/05/23 01:00:07 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+    {
+    Weapon club = Weapon("crude spiked club");
+    HumanA bob("Bob", club);
+    bob.attack();
+    club.setType("some other type of club");
+    bob.attack();
+    }
+    {
+    Weapon club = Weapon("crude spiked club");
+    HumanB jim("Jim");
+    jim.setWeapon(club);
+    jim.attack();
+    club.setType("some other type of club");
+    jim.attack();
+    }
+
+    return (0);
+}
