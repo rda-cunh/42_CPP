d4a1b9e7d3ff8959ac7ad5b5afee13da8e9f9ef3
rda-cunh <ricard.mendes@gmail.com>
2025-05-24 00:09:06 +0100

    final version os ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
 
 void checkArgs(int argc, char **argv)
 {
@@ -29,26 +30,40 @@ void checkArgs(int argc, char **argv)
     }
 }
 
+std::string findAndReplace(std::string toFind, std::string toReplace, std::string line)
+{
+    size_t      startPos = 0;
+
+    while ((startPos = line.find(toFind, startPos)) != std::string::npos)
+    {
+        line.erase(startPos, toFind.length());
+        line.insert(startPos, toReplace);
+        startPos += toReplace.length();
+    }
+    return (line);
+}
+
 int main(int argc, char **argv)
 {
     checkArgs(argc, argv);
-    std::ifstream inFile(argv[1]);
-    if (!inFile.is_open())
-    {
-        std::cout << "File is open and cannot be read." << std::endl;
-        return (1); 
-    }
+    std::ifstream inFile;
+    inFile.open(argv[1], std::ios::in);
+    if (inFile.fail())
+        return (std::cout << "File annot be open." << std::endl, 1); 
     else
     {
         std::ofstream outFile;
         std::string inFileName = argv[1];
         inFileName += ".replace";
-        outFile.open(outFile.c_str(), std::ios::out);
+        outFile.open(inFileName.c_str(), std::ios::out);
+        std::string line;
         if (outFile.is_open())
         {
-
+            while (getline(inFile, line))
+                outFile << findAndReplace(argv[2], argv[3], line) << std::endl;
         }
         inFile.close();
         outFile.close();
     }
+    return (0);
 }
