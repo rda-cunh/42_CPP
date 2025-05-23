05a2803a36bb2b57ac67721fdfb77461892086d9
rda-cunh <ricard.mendes@gmail.com>
2025-05-23 19:51:38 +0100

    start implemeting ex04 logic

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -1,7 +1,18 @@
-//missing 42 header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 18:37:01 by rda-cunh          #+#    #+#             */
+/*   Updated: 2025/05/23 18:37:01 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 void checkArgs(int argc, char **argv)
 {
@@ -21,5 +32,23 @@ void checkArgs(int argc, char **argv)
 int main(int argc, char **argv)
 {
     checkArgs(argc, argv);
-    std::string 
+    std::ifstream inFile(argv[1]);
+    if (!inFile.is_open())
+    {
+        std::cout << "File is open and cannot be read." << std::endl;
+        return (1); 
+    }
+    else
+    {
+        std::ofstream outFile;
+        std::string inFileName = argv[1];
+        inFileName += ".replace";
+        outFile.open(outFile.c_str(), std::ios::out);
+        if (outFile.is_open())
+        {
+
+        }
+        inFile.close();
+        outFile.close();
+    }
 }
