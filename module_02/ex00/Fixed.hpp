aac5f7146d8ed8766a81f5217601d928ac223fe9
rda-cunh <ricard.mendes@gmail.com>
2025-05-25 23:59:03 +0100

    added guards to hpp file

--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -6,7 +6,15 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 23:48:59 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/05/25 23:49:02 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/05/25 23:58:35 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+
+
+
+
+#endif
