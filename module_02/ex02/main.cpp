00098fee19dbd4f92129c63c5b91ddefc41f2e50
rda-cunh <ricard.mendes@gmail.com>
2025-05-29 16:59:25 +0100

    ex02: included operator overloading

--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -6,7 +6,26 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 23:50:47 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/05/25 23:50:50 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/05/29 15:22:26 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main(void) 
+{
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+    
+    std::cout << Fixed::max( a, b ) << std::endl;
+
+    return (0);
+}
