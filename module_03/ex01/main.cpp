63a655713b969f0b7fc730237b861bc2933fcfbf
rda-cunh <ricard.mendes@gmail.com>
2025-06-06 16:16:25 +0100

    ex00: added setName and commented attack between ClapTraps (as subject demands no interaction)

--- module_03/ex01/main.cpp
+++ module_03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 15:14:32 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/06/06 16:14:36 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/06/06 08:51:00 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int main()
 {
     std::cout << "--- TEST 1: Creating a Claptrap, attacking and reparing ---" << std::endl;
-    std::cout << std::endl;
 
+    std::cout << std::endl;
     ClapTrap RoboCop("Robocop");
     RoboCop.attack("aliens");
     RoboCop.takeDamage(3);
@@ -26,30 +26,21 @@ int main()
     std::cout << std::endl;
     std::cout << "RoboCop is loosing and decide to create a clone...\n" << std::endl;
 
-    std::cout << "--- TEST 2: Creating a copy Claptrap, attack, clone died and threat elimination ---" << std::endl;
+    std::cout << "--- TEST 2: Creating a copy Claptrap, attack, friendly attack, clone died and threat elimination ---" << std::endl;
+    
     std::cout << std::endl;
-
     ClapTrap RoboCop2(RoboCop);
-    RoboCop2.setName("RoboCop Clone");
     RoboCop2.attack("aliens");
     RoboCop2.takeDamage(5);
     std::cout << std::endl;
-
-    /*
     std::cout << "RoboCop clone missed and atacked Robocop:" << std::endl;
     RoboCop2.attack(RoboCop.getName());   //friendly attack
-    std::cout << std::endl;
-    */        
-
-    std::cout << "RoboCop & RoboCop Clone just suffered some more damage:" << std::endl;
     RoboCop.takeDamage(3);
     RoboCop2.takeDamage(5);
-    
     std::cout << std::endl;
     std::cout << "RoboCop clone died, but will atempt to atack and repair:" << std::endl;
     RoboCop2.attack("aliens");
     RoboCop2.beRepaired(10);
-    
     std::cout << std::endl;
     RoboCop.attack("aliens");
     RoboCop.attack("aliens");
