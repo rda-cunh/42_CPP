d777c71220ec6e999229801e706bdf0e10ae0f1e
rda-cunh <ricard.mendes@gmail.com>
2025-07-01 20:36:00 +0100

    ex00: projects structure and Buro class

---
+++ module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 19:38:11 by rda-cunh          #+#    #+#             */
+/*   Updated: 2025/07/01 20:35:26 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+    private:
+        const std::string   _name;
+        int                 _grade;              
+
+    public:
+        Bureaucrat();                                   //default contructor
+        Bureaucrat(const Bureaucrat &other);            //default copy contructor
+        Bureaucrat &operator=(const Bureaucrat &other); //copy assignement operator
+        ~Bureaucrat();                                  //destructor 
+        
+        std::string getName();
+        int         getgrade();
+};
+
+#endif
