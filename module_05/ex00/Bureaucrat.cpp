2b92f602bf6c0dfffcc477ec1fa1e151dbab37a0
rda-cunh <ricard.mendes@gmail.com>
2025-07-07 19:45:16 +0100

    ex00: continued class buro implmentation

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -6,9 +6,52 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 19:38:23 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/07/01 20:26:01 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/07/07 19:31:43 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+    std::cout << "Bureaucrat: standard constructor called." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int const &grade) : _name(name)
+{
+    std::cout << "Bureaucrat: constructor called." << std::endl;
+    if (grade < MIN_GRADE)
+        throw Bureaucrat::GradeTooHighException();
+    else if (grade > MAX_GRADE)
+        throw Bureaucrat::GradeTooLowException();
+    this->_grade = grade; 
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade() 
+{
+    std::cout << "Bureaucrat: copy constructor called." << std::endl;
+    *this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    std::cout << "Bureaucrat: copy assignement operator called." << std::endl;
+    if (this ==  &other)
+        return (*this);
+    this->_grade = other._grade;
+    return (*this);    
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat: destructor called." << std::endl;
+}
+
+std::string Bureaucrat::getName() {return(this->_name);}
+
+int Bureaucrat::getgrade() {return(this->_grade);}
+
+void Bureaucrat::incrementBuro()
+{
+    if (_grade - 1 < MAX_GRADE)
+}
--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 19:38:11 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/07/01 21:14:06 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/07/07 19:41:59 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <iostream>
 #include <string>
 
+#define MAX_GRADE 150
+#define MIN_GRADE 1
+
 class Bureaucrat
 {
     private:
@@ -23,15 +26,27 @@ class Bureaucrat
         int                 _grade;              
 
     public:
-        Bureaucrat();                                   //default contructor
-        Bureaucrat(const Bureaucrat &other);            //default copy contructor
-        Bureaucrat &operator=(const Bureaucrat &other); //copy assignement operator
-        ~Bureaucrat();                                  //destructor 
+        Bureaucrat();                                           //default contructor
+        Bureaucrat(std::string const &name, int const &grade);
+        Bureaucrat(const Bureaucrat &other);                    //default copy contructor
+        Bureaucrat &operator=(const Bureaucrat &other);         //copy assignement operator
+        ~Bureaucrat();                                          //destructor 
         
         std::string getName();
         int         getgrade();
         void        incrementBuro();
         void        decrementBuro();
+
+        class GradeTooHighException : public std::exception
+        {
+            virtual const char *what() const throw(); //verificar a necessidade do virtual: https://cplusplus.com/doc/tutorial/exceptions/ 
+        };
+        class GradeTooLowException : public std::exception
+        {
+            virtual const char *what() const throw(); //verificar a necessidade do virtual 
+        };
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &obj); //operator overloading to allow printing
 
 #endif
