9ca60b12d2e8255e7a8f212b1db8627b36c3892d
rda-cunh <ricard.mendes@gmail.com>
2025-08-12 08:08:16 +0100

    ex00: added corretions into templates and main from subject

--- module_07/ex00/inc/templates.hpp
+++ module_07/ex00/inc/templates.hpp
@@ -6,12 +6,12 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 07:44:33 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/11 07:54:36 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/08/12 08:01:29 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEMPLATES_HPP
-# define TEMPLATES.HPP
+# define TEMPLATES_HPP
 
 template <typename T>
 void swap(T &a, T &b)
@@ -19,18 +19,18 @@ void swap(T &a, T &b)
     T temp = a;
     a = b;
     b = temp;
-};
+}
 
 template <typename T>
-T min(T &a, T &b)
+const T &min(T const &a, T const &b)
 {
-    return( a < b ? a : b)
-};
+    return( a < b ? a : b);
+}
 
 template <typename T>
-T mzx(T &a, T &b)
+const T &max(T const &a, T const &b)
 {
-    return( a > b ? a : b)
-};
+    return( a > b ? a : b);
+}
 
 #endif
