8651d41ef92f1d975d3cb7cd4e4eea5388a63395
rda-cunh <ricard.mendes@gmail.com>
2025-08-15 17:34:29 +0100

    ex01: completed exercises and introduced additional tests

--- module_07/ex01/inc/iter.hpp
+++ module_07/ex01/inc/iter.hpp
@@ -6,14 +6,20 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:57:56 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/12 17:00:48 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/08/15 16:48:57 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef>
 
-
+template <typename T>
+void iter(T *arrayAdress, size_t arrayLen, void(*function)(T&))
+{
+    for(size_t i = 0; i < arrayLen; i++)
+        function(arrayAdress[i]);
+}
 
 #endif
--- module_07/ex01/src/main.cpp
+++ module_07/ex01/src/main.cpp
@@ -6,7 +6,41 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:57:45 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/12 16:57:48 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/08/15 17:32:40 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+void ft_printChar(char &c) { std::cout << c << std::endl; }
+void ft_doubleNum(int &n) 
+{ 
+    n *= 2; 
+    std::cout << n << std::endl;
+}
+void ft_moduleFloats(float &f)
+{
+    f = (f < 0 ? -f : f); 
+    std::cout << f << std::endl;
+}
+
+int main()
+{
+    std::cout << "*** Test 1: print each char of a string (array of chars) ***" << std::endl;
+    std::string str = "Hello World!";
+    ::iter(&str[0], str.size(), ft_printChar);
+    std::cout << std::endl;
+
+    std::cout << "*** Test 2: double the numbers of an int array ***" << std::endl;
+    int array[] = {1, 2, 3, 4, 5};
+    ::iter(&array[0], sizeof(array)/sizeof(array[0]), ft_doubleNum);
+    std::cout << std::endl;
+
+    std::cout << "*** Test 3: apply module on an array of floats ***" << std::endl;
+    float arrayf[] = {-33.0f, 45.06f, -88.787f};
+    ::iter(&arrayf[0], sizeof(arrayf)/sizeof(arrayf[0]), ft_moduleFloats);
+
+    return (0);
+}
