dcfe80e6c5309fbd77906551b006b33b13650ebe
rda-cunh <ricard.mendes@gmail.com>
2025-08-19 14:18:48 +0100

    ex01: added an aditional test (not working yet -- to debug latter)

--- module_07/ex01/inc/iter.hpp
+++ module_07/ex01/inc/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:57:56 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/15 16:48:57 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/08/19 14:13:09 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 #include <cstddef>
 
-template <typename T>
-void iter(T *arrayAdress, size_t arrayLen, void(*function)(T&))
+template <typename T, typename F>
+void iter(T *arrayAdress, size_t arrayLen, F function) //can use void(*function)(T const &) or void(*function)(T&) instead if do not want to use a template for the function and define previously if the array can be changed
 {
     for(size_t i = 0; i < arrayLen; i++)
         function(arrayAdress[i]);
