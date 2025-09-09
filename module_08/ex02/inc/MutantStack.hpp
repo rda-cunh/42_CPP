a5e314ebd5a96f6f5a87fd7563fe1d6b6a8cc9f9
rda-cunh <ricard.mendes@gmail.com>
2025-09-09 15:29:23 +0100

    ex02: completed exercise (untested)

--- module_08/ex02/inc/MutantStack.hpp
+++ module_08/ex02/inc/MutantStack.hpp
@@ -6,7 +6,32 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 07:54:44 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/22 07:54:46 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/09 15:26:03 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <deque>
+
+template <typename T, typename C = std::deque<T> >
+class MutantStack : public std::stack<T, C>
+{
+    public:
+        MutantStack();
+        MutantStack(const MutantStack &other);
+        MutantStack &operator=(const MutantStack &other);
+        ~MutantStack();
+
+        typedef typename C::iterator iterator;
+        typedef typename C::const_iterator const_iterator;
+
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
+};
+
+#include "MutantStack.tpp"
--- module_08/ex02/src/main.cpp
+++ module_08/ex02/src/main.cpp
@@ -6,7 +6,34 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 07:53:43 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/08/22 07:53:44 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/09 15:09:35 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
+}
