7b12a63ade00874336b1e2fd567a4599a6162dfd
rda-cunh <ricard.mendes@gmail.com>
2025-09-17 00:10:31 +0100

    ex00: class first attempt

--- module_09/ex00/inc/BitcoinExchange.hpp
+++ module_09/ex00/inc/BitcoinExchange.hpp
@@ -6,6 +6,32 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 15:59:58 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/09/10 16:00:01 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/17 00:07:28 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+
+class BitcoinExchange
+{
+private:
+    std::map<std::string, float> _list;
+
+    BitcoinExchange(); // Default constructor (non-instantiable class).
+
+public:
+    BitcoinExchange(const char *file);                        // Constructor
+    BitcoinExchange(const BitcoinExchange &other);            // Copy Constructor
+    BitcoinExchange &operator=(const BitcoinExchange &other); // Copy assignement operator
+    ~BitcoinExchange();                                       // Destructor
+
+    // Exceptions
+    class NoDataFile : public std::exception
+    {
+        const char *what() const throw();
+    };
+};
