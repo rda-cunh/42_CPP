fe6fe75e07b2d33a1c3216cc53b58643a1a4da28
rda-cunh <ricard.mendes@gmail.com>
2025-09-22 12:38:16 +0100

    ex01: first class version

--- module_09/ex01/src/main.cpp
+++ module_09/ex01/src/main.cpp
@@ -6,6 +6,20 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:05:12 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/09/10 16:05:14 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/22 12:24:06 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "RPN.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Please provide a correct argument. Run as ./RPN argument"
+                  << std::endl;
+        return (1);
+    }
+    RPN(argv[1]);
+    return (0);
+}
