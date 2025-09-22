9a1b194effbf02442dcbe9c98837a66fad958d6d
rda-cunh <ricard.mendes@gmail.com>
2025-09-22 18:24:00 +0100

    ex01: constructor implementation

--- module_09/ex01/src/RPN.cpp
+++ module_09/ex01/src/RPN.cpp
@@ -6,6 +6,37 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:05:32 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/09/10 16:05:34 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/22 18:23:22 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "RPN.hpp"
+
+RPN::RPN() {}
+
+RPN::RPN(char *input) : _input(input)
+{
+    float a;
+    float b;
+
+    for (int i = 0; i < _input.size(); i++)
+    {
+        if (std::isdigit(_input[i]))
+            this->_stack.push(std::strtof(&_input[i], NULL));
+        else if (_input.std::string::find_first_of("+-*/", i) == i)
+        {
+            if (this->_stack.size() < 2)
+                throw NotEnoughOperands();
+            b = this->_stack.top();
+            this->_stack.pop();
+            a = this->_stack.top();
+            this->_stack.pop();
+
+            if (_input[i] == '+')
+                this->_stack.push(a + b);
+            else if (_input[i] == '-')
+                this->_stack.push(a - b);
+            else if (_input[i] == '-')
+        }
+    }
+}
