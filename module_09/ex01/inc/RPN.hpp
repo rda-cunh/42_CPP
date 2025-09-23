fdaab92e88878a5b9aadd77d3e1303f2544bdd42
rda-cunh <ricard.mendes@gmail.com>
2025-09-23 19:30:02 +0100

    ex01: final version (working but untested)

---
+++ module_09/ex01/inc/RPN.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 16:05:42 by rda-cunh          #+#    #+#             */
+/*   Updated: 2025/09/23 18:43:17 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cstdlib>
+
+class RPN
+{
+private:
+    std::string _input;
+    std::stack<float> _stack;
+    RPN();
+
+public:
+    RPN(char *input);
+    RPN(const RPN &other);
+    RPN &operator=(const RPN &other);
+    ~RPN();
+
+    class DivisionByZero : public std::exception
+    {
+        const char *what() const throw();
+    };
+
+    class NotEnoughOperands : public std::exception
+    {
+        const char *what() const throw();
+    };
+
+    class NotEnoughOperators : public std::exception
+    {
+        const char *what() const throw();
+    };
+};
--- module_09/ex01/src/RPN.cpp
+++ module_09/ex01/src/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:05:32 by rda-cunh          #+#    #+#             */
-/*   Updated: 2025/09/22 18:23:22 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2025/09/23 19:27:59 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ RPN::RPN(char *input) : _input(input)
     float a;
     float b;
 
-    for (int i = 0; i < _input.size(); i++)
+    for (size_t i = 0; i < _input.size(); i++)
     {
         if (std::isdigit(_input[i]))
             this->_stack.push(std::strtof(&_input[i], NULL));
-        else if (_input.std::string::find_first_of("+-*/", i) == i)
+        else if (_input.std::string::find_first_of("+-/*", i) == i)
         {
             if (this->_stack.size() < 2)
                 throw NotEnoughOperands();
@@ -37,14 +37,47 @@ RPN::RPN(char *input) : _input(input)
             else if (_input[i] == '-')
                 this->_stack.push(a - b);
             else if (_input[i] == '/')
+            {
                 if (b == 0)
-                    throw DivideByZero();
+                    throw DivisionByZero();
                 this->_stack.push(a / b);
+            }
             else if (_input[i] == '*')
                 this->_stack.push(a * b);
         }
     }
-    if (_stack.size() !0 2)
+    if (_stack.size() != 1)
         throw NotEnoughOperators();
     std::cout << "The result is: " << _stack.top() << std::endl;
+}
+
+RPN::RPN(const RPN &other)
+{
+    *this = other;
+}
+
+RPN &RPN::operator=(const RPN &other)
+{
+    if (this == &other)
+        return (*this);
+    this->_input = other._input;
+    this->_stack = other._stack;
+    return (*this);
+}
+
+RPN::~RPN() {}
+
+const char *RPN::DivisionByZero::what() const throw()
+{
+    return ("Error: Division by zero is not possible.");
+}
+
+const char *RPN::NotEnoughOperands::what() const throw()
+{
+    return ("Error: Not enough operands.");
+}
+
+const char *RPN::NotEnoughOperators::what() const throw()
+{
+    return ("Error: Not enough operators.");
 }
