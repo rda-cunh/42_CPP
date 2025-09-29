ca45cc228538465e799d844c5d9f06e72ba2e827
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2025-09-29 14:16:10 +0100

    ex02: main and Makefile updated

--- module_09/ex02/src/main.cpp
+++ module_09/ex02/src/main.cpp
@@ -9,3 +9,24 @@
 /*   Updated: 2025/09/10 16:14:54 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error: Please provide a correct argument. Run as " <<
+            "./PmergeMe argument" << std::endl;
+        return (1);
+    }
+    try
+    {
+        PmergeMe sort(argv[1]);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
+}
